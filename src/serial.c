0e3241af7562a9ceeb0c6f049079647cde0b0767
shidozz <shidozzmusic@gmail.com>
2023-06-07 17:15:16 +0200

    Update serial.c

--- src/serial.c
+++ src/serial.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdarg.h>
-#include "../include/mmu.h"
+#include "../include/paging.h"
 #include "../include/serial.h"
 #include "../include/string.h"
 // Déclaration des registres UART
