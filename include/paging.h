1656bd3f35ce27febecdba16b2dd72740e6cb887
shidozz <shidozzmusic@gmail.com>
2023-06-07 17:12:55 +0200

    Update paging.h

--- include/paging.h
+++ include/paging.h
@@ -2,4 +2,15 @@
 #define __PAGING_H__
 #include <stdint.h>
 
+// Structure d'entrée de table des pages
+typedef struct {
+    uint64_t valid: 1;
+    uint64_t address: 48;
+    // Autres attributs de contrôle des pages (présents, permission, etc.)
+    // ...
+} PageTableEntry;
+
+void map_pages();
+void initialize_page_table();
+void enable_pagination();
 #endif
